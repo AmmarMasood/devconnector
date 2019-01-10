c3d4d12b0878aba336645620a90f5be79e4c4c62
AmmarMasood <ammar.masood98@gmail.com>
2019-01-10 23:43:50 +0500

    prepare for deplyment

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require("./models/Profile.js");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const app = express();
 
@@ -31,6 +32,16 @@ require("./config/passport")(passport);
 require("./routes/api/users")(app);
 require("./routes/api/profile")(app);
 require("./routes/api/posts")(app);
+
+//server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get(`*`, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 //calling the port
 const port = process.env.PORT || 5000;
